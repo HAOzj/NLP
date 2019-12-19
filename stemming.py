c315557445ffc2aa211d0354ce7a9adcec1b1e32
HAO Zhaojun <haozhaojun@eisti.eu>
2019-12-19 15:45:37 +0800

    增加说明

--- stemming.py
+++ stemming.py
@@ -9,6 +9,13 @@ import re
 
 
 def stemmer(title: str, min: int) -> str:
+    """提取漫画标题的主干,称为stem
+
+    比如 "蓝猫淘气第三部"提取出"蓝猫淘气
+    Args:
+        title(str) :- 标题
+        min(int) :- stem的最小长度
+    """
     pattern = r"(?P<deliminator>第[0-9一二三四五六七八九十][部季章]|之)"
     delimeator = re.search(pattern=pattern, string=title)
     start_index = len(title)
